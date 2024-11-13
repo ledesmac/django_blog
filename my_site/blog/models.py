efa517bf482e4994c0957bf5c65069710e44640e
Alex Ledesma <alex.ledesma7@gmail.com>
2024-11-13 11:39:05 -0500

    updating models.py

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.caption
 class Author (models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
     
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -16,13 +25,13 @@ class Author (models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=350)
     img_name  = models.CharField(max_length=100)
-    date = models.DateField()
-    slug = models.SlugField(default="", blank= True, null=False, db_index=True) 
-    content = models.CharField(max_length=10000)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    
-
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, default="", blank= True, null=False, db_index=True) 
+    content = models.TextField(validators=[MinLengthValidator])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
     
